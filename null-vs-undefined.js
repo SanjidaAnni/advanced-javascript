e7627ec76bcac34eaf1330598791ed2f32c9e95d
unknown <sanjida.anni2@gmail.com>
2021-02-02 12:26:17 +0600

    true-false done

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const student = {name:"ridima", phone:97487}
+console.log(student.name);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
+
+null = not existence;
---
+++ true-false.js
@@ -0,0 +1,16 @@
+// Falsy:
+// false
+// 0
+// ""
+// undefined
+// NaN
+// Truthy
+// '0', ' ' []
+let name = 0;
+if(name  || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+} 
+
