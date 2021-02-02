e56ec2a0b9daffc8ecd2bcd06b44d1cd4ec6c132
unknown <sanjida.anni2@gmail.com>
2021-02-02 12:55:50 +0600

    double tripple done

--- double-vs-tripple-equal.js
+++ double-vs-tripple-equal.js
@@ -1,8 +1,20 @@
-const first = 0;
-const second = false;
+const first = 1;
+const second = true;
+
+//doubole equal check only equality of values
 if(first == second){
     console.log("condition is true");
 }
-else{
-    console.log("condition is false");
+else {
+    console.log("condition is false")
 }
+
+//triple equal check equality of value and type
+if(first === second){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false")
+}
+
+//so for compare we will always use ===
